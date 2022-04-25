9bc28ab9801034b19843f77beb9fa3fae29a49cb
LAPTOP-UMB3FD8A\Wow <qyyzxty@icloud.com>
2022-04-25 21:12:48 +0800

    normalized code

---
+++ week01/LinkList/LinkList/LinkList.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+using namespace std;
+
+// 链表结点的定义
+struct LinkNode {
+    int val;
+    LinkNode* next;
+    LinkNode() : val(0), next(nullptr) {}
+    LinkNode(int x) : val(x), next(nullptr) {}
+    LinkNode(int x, LinkNode* next) : val(x), next(next) {}
+};
+
+class KLinkSolution {
+public:
+    KLinkSolution();
+    ~KLinkSolution();
+
+    LinkNode* reverseKGroup(LinkNode* head, int k) {
+        //...
+    }
+
+private:
+
+};
+
+KLinkSolution::KLinkSolution()
+{
+}
+
+KLinkSolution ::~KLinkSolution()
+{
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
